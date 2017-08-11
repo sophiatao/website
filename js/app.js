1c179c38207889348f24cf210de8b4c4b3739198
inksplatt <30760803+sophiatao@users.noreply.github.com>
2017-08-11 13:42:06 -0400

    Finalize responsive navbar, fix content overflow errors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* responding to width change*/
+
 const mq = window.matchMedia('(max-width: 991px)');
 
 mq.addListener(WidthChange);
@@ -5,12 +7,20 @@ mq.addListener(WidthChange);
 function WidthChange(mq) {
     if (mq.matches) {
         $('nav').removeClass('affix');
-        $('nav').addClass('upper');
         $('li').css('display', 'inline');
     }
     else {
         $('nav').addClass('affix');
-        $('nav').removeClass('upper');
         $('li').css('display','');
     }
 };
+
+/* navigating*/
+$(document).ready(function() {
+    $('a').click(function(evt) {
+        var anchor = $(this).attr('href');
+        $('article').addClass('hidden');
+        $(anchor).removeClass('hidden');
+
+    });
+});
